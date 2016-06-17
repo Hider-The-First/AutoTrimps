f7ef491a2b6fb4ba687851fa2cff6b7da2f722ee
Hider-The-First <orenvilderman@walla.co.il>
2016-06-17 05:41:08 +0300

    Update Null run version.js

--- Null run version.js
+++ Null run version.js
@@ -1865,35 +1865,35 @@ function autoMap() {
                 }
                 if(theMap.name == 'Bionic Wonderland' && (game.global.challengeActive == "Crushed" || getPageSetting('VoidMaps') > 200)) {
                     var wonderlandDifficulty = Math.ceil(theMap.difficulty / 2);
-                    if(game.global.world = 125 && game.global.mapBonus < 1) {
+                    if(game.global.world == 125 && game.global.mapBonus < 1) {
                         shouldDoMap = theMap.id;
                         break;
                     }
                 }
                 if(theMap.name == 'Bionic Wonderland II' && getPageSetting('VoidMaps') > 200) {
                     var wonderlandDifficulty = Math.ceil(theMap.difficulty / 2);
-                    if(game.global.world >= 140 && game.global.mapBonus < 1) {
+                    if(game.global.world == 140 && game.global.mapBonus < 1) {
                         shouldDoMap = theMap.id;
                         break;
                     }
                 }
                 if(theMap.name == 'Bionic Wonderland III' && getPageSetting('VoidMaps') > 200) {
                     var wonderlandDifficulty = Math.ceil(theMap.difficulty / 2);
-                    if(game.global.world >= 155 && game.global.mapBonus < 1) {
+                    if(game.global.world == 155 && game.global.mapBonus < 1) {
                         shouldDoMap = theMap.id;
                         break;
                     }
                 }
                 if(theMap.name == 'Bionic Wonderland IV' && getPageSetting('VoidMaps') > 200) {
                     var wonderlandDifficulty = Math.ceil(theMap.difficulty / 2);
-                    if(game.global.world >= 170 && game.global.mapBonus < 1) {
+                    if(game.global.world == 170 && game.global.mapBonus < 1) {
                         shouldDoMap = theMap.id;
                         break;
                     }
                 }
                 if(theMap.name == 'Bionic Wonderland V' && getPageSetting('VoidMaps') > 200) {
                     var wonderlandDifficulty = Math.ceil(theMap.difficulty / 2);
-                    if(game.global.world >= 185 && game.global.mapBonus < 1) {
+                    if(game.global.world == 185 && game.global.mapBonus < 1) {
                         shouldDoMap = theMap.id;
                         break;
                     }
