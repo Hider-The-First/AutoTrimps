ffaad631af119a30f48233485d2f92131e794fa1
Hider-The-First <orenvilderman@walla.co.il>
2016-06-19 23:45:57 +0300

    Update Null run version.js

--- Null run version.js
+++ Null run version.js
@@ -1069,9 +1069,9 @@ function buyStorage() {
             document.getElementById('Prestige').selectedIndex = 13;
             autoTrimpSettings.Prestige.selected = "GambesOP";
        }
-       if (game.global.world < 200 && game.global.world != 31 && game.global.world != 32 && game.global.world != 33 && game.global.world != 34 && game.global.world != 35 && game.global.world != 36) {
-           document.getElementById('Prestige').selectedIndex = 2;
-           autoTrimpSettings.Prestige.selected = "Dagadder";
+       if (game.global.world < 200) { //&& game.global.world != 31 && game.global.world != 32 && game.global.world != 33 && game.global.world != 34 && game.global.world != 35 && game.global.world != 36) {
+           document.getElementById('Prestige').selectedIndex = 4;
+           autoTrimpSettings.Prestige.selected = "Megamace";
        } else if (game.global.world != 200 && game.global.world != 201) {
            document.getElementById('Prestige').selectedIndex = 0;
            autoTrimpSettings.Prestige.selected = "Off";
