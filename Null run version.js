07cfa9fc51cce94254ef5c7607c76d986fc30784
Hider-The-First <orenvilderman@walla.co.il>
2016-06-22 03:44:43 +0300

    Update Null run version.js

--- Null run version.js
+++ Null run version.js
@@ -1769,9 +1769,9 @@ function autoMap() {
         }
         //here we start doing maps for null its fun (and also for spire)
         var shouldDoNullMaps = false;
-        if (game.global.mapBonus < 9 && game.global.world > 165 && game.global.world != 200) {
-        //(game.global.world == 200 && ((new Date().getTime() - game.global.zoneStarted) / 1000 / 60) < 20 && game.global.lastClearedCell > 20) ||
-        //(game.global.world == 200 && ((new Date().getTime() - game.global.zoneStarted) / 1000 / 60) < 25 && game.global.lastClearedCell > 60)) {
+        if ((game.global.mapBonus < 9 && game.global.world > 165 && game.global.world != 200) ||
+        (game.global.world == 200 && ((new Date().getTime() - game.global.zoneStarted) / 1000 / 60) < 10 && game.global.lastClearedCell > 20) ||
+        (game.global.world == 200 && ((new Date().getTime() - game.global.zoneStarted) / 1000 / 60) < 15 && game.global.lastClearedCell > 60)) {
         //(game.global.mapBonus < 1 && (game.global.world == 125) && game.global.lastClearedCell > 81) ||
         //(game.global.mapBonus < 9 && game.global.world > 208)) {
             shouldDoMaps = true;
