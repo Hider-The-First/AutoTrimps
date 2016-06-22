cf41ba45ad956e02f480df4575f3e9f4c74f9efe
Hider-The-First <orenvilderman@walla.co.il>
2016-06-22 04:00:04 +0300

    Update Null run version.js

--- Null run version.js
+++ Null run version.js
@@ -1772,6 +1772,7 @@ function autoMap() {
         if ((game.global.mapBonus < 2 && game.global.world > 165 && game.global.world < 171 && game.global.world != 200) ||
         (game.global.mapBonus < 5 && game.global.world > 170 && game.global.world < 201 && game.global.world != 200) ||
         (game.global.mapBonus < 9 && game.global.world > 180 && game.global.world < 191 && game.global.world != 200) ||
+        (game.global.mapBonus < 9 && game.global.world == 200) ||
         (game.global.world == 200 && ((new Date().getTime() - game.global.zoneStarted) / 1000 / 60) < 10 && game.global.lastClearedCell > 20) ||
         (game.global.world == 200 && ((new Date().getTime() - game.global.zoneStarted) / 1000 / 60) < 15 && game.global.lastClearedCell > 60)) {
         //(game.global.mapBonus < 1 && (game.global.world == 125) && game.global.lastClearedCell > 81) ||
