8eb83ee87ebc4585cbab3cc9567f1a1c7c32cb3f
Hider-The-First <orenvilderman@walla.co.il>
2016-08-08 20:16:03 +0300

    Update LateGameTestings.js

--- LateGameTestings.js
+++ LateGameTestings.js
@@ -1537,7 +1537,7 @@ function autoLevelEquipment() {
                     ||
                     ( getPageSetting('BuyWeaponUpgrades') && equipmentList[equipName].Stat == 'block' )
                     ||
-                    ((getPageSetting('BuyArmorUpgrades') && ((equipmentList[equipName].Resource != 'metal') || ((gameResource.prestige+2 <= ((game.global.world-5)/5)) || (gameResource.prestige+1 <= ((game.global.world-5)/5) && game.global.world > getPageSetting('VoidMaps'))) || gameResource.prestige < 5) && (equipmentList[equipName].Stat == 'health'))
+                    ((getPageSetting('BuyArmorUpgrades') && ((equipmentList[equipName].Resource != 'metal') || ((gameResource.prestige+2 <= ((game.global.world-5)/5)) || (gameResource.prestige+2 <= ((game.global.world-5)/5) && game.global.world > getPageSetting('VoidMaps'))) || gameResource.prestige < 5) && (equipmentList[equipName].Stat == 'health'))
                         && 
                 //Only buy Armor prestiges when 'DelayArmorWhenNeeded' is on, IF:
                         (
