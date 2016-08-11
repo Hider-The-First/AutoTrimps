3867a8acb3b9d631776e25b96b319837edd09ef3
Hider-The-First <orenvilderman@walla.co.il>
2016-08-11 07:47:04 +0300

    Update LateGameTestings.js

--- LateGameTestings.js
+++ LateGameTestings.js
@@ -1032,7 +1032,7 @@ function highlightHousing() {
                     if (game.buildings.Warpstation.owned >= (Math.floor(game.upgrades.Gigastation.done * getPageSetting('DeltaGigastation')) + getPageSetting('FirstGigastation')))
                         bestBuilding = null;
                 }
-                if (getPageSetting('WarpstationWall') && bestBuilding == "Warpstation") {
+                if (game.global.world >= getPageSetting('VoidMaps') || getPageSetting('WarpstationWall') && bestBuilding == "Warpstation") {
                     //Warpstation Wall - allow only warps that cost 1/n'th less then current metal (try to save metal for next prestige) 
                     var costratio = 4;  //(1/4th)                    
                     if (getBuildingItemPrice(game.buildings.Warpstation, "metal", false, 1) * Math.pow(1 - game.portal.Resourceful.modifier, game.portal.Resourceful.level) > game.resources.metal.owned/costratio)
