9cd7716e3e060bdf25bc4d146f1148cea2af977d
Hider-The-First <orenvilderman@walla.co.il>
2016-08-15 02:55:37 +0300

    Update LateGameTestings.js

--- LateGameTestings.js
+++ LateGameTestings.js
@@ -2003,15 +2003,22 @@ function autoMap() {
         if (game.talents.hyperspeed.purchased) {
         	cellClearTime -= 50
         }
+        //levels of new Giga
+        var stationLevel = [61,62,63,64,65,66,67,68,69,70,72,74,76,78,81,84,87,90,95,100,105,110,115,120,125,130,135,140,145,150,155,160,165,170,175,180,190,200,210,220,230,240,250,260,270,280,290,300];
+        for (var stopToBuild in stationLevel) {
+        stopToBuild = stationLevel[stopToBuild];
         if (		
         //enter map in zones if you miss to overkill, the script will make sure you farm more if you can't overkill in the map.
         (game.global.mapBonus < 1 && game.global.world >= 11 && OVKcellsWorld+1 < game.global.lastClearedCell/2) ||
         //force to stay in mapYouSlow if you overkill all the cells unless you are about to hit max map bonus.
         (game.global.world >= 15 && game.global.mapsActive && game.global.mapBonus < 9 && (new Date().getTime() - game.global.mapStarted) > (cellClearTime * game.global.mapGridArray.length)) ||
-        //option to force stay in zone X time in min/cleared maps and farm
+        //Clear maps to unlcok nurseries for genetics.
         (game.global.world == 50 && game.global.mapBonus < 1) ||
         (game.global.world == 90 && game.global.mapBonus < 1) ||
-        (game.global.world == 199 && game.global.mapBonus < 1)
+        (game.global.world == 199 && game.global.mapBonus < 1)||
+        //do maps before new gigastation
+        (game.global.world == stopToBuild && game.global.mapBonus < 1)
+        //option to force stay in zone X time in min/cleared maps and farm
         //(game.global.world >= 310 && ((new Date().getTime() - game.global.zoneStarted) / 1000 / 60) < 10)         
         //option to force stay in zone X time in min and farm		
         //(game.global.world == 200 && game.global.lastClearedCell > 20 && ((new Date().getTime() - game.global.zoneStarted) / 1000 / 60) < 10)		
