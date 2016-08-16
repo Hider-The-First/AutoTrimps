c03040e0460aa316ec5162bea7f895df0ddb860b
Hider-The-First <orenvilderman@walla.co.il>
2016-08-16 21:50:34 +0300

    Update LateGameTestings.js

--- LateGameTestings.js
+++ LateGameTestings.js
@@ -930,7 +930,7 @@ function initializeAutoTrimps() {
     loadPageVariables();
 
     var atscript = document.getElementById('AutoTrimps-script')
-      , base = 'https://github.com/Hider-The-First/AutoTrimps'
+      , base = 'https://rawgit.com/Hider-The-First/AutoTrimps/patch-3'
       ;
     if (atscript !== null) {
     }
