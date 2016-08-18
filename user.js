bc864c3f3cf04059b04b9f1d520d91dcb6a76e3e
Hider-The-First <orenvilderman@walla.co.il>
2016-08-18 10:57:38 +0300

    Update user.js

--- user.js
+++ user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         AutoTrimps-genBTC
-// @namespace    https://github.com/genbtc/AutoTrimps
-// @version      2.1
-// @description  Automate all the trimps!
-// @author       zininzinin, spindrjr, Ishkaru, genBTC
+// @name         AutoAutoTrimps-hider
+// @namespace    https://github.com/Hider-The-First/AutoTrimps
+// @version      2.1.3H-hider-17-8-2016
+// @description  Automate the AutoTrimps!
+// @author       zininzinin, spindrjr, Ishkaru, genBTC, hider
 // @include        *trimps.github.io*
 // @include        *kongregate.com/games/GreenSatellite/trimps
 // @grant        none
 // ==/UserScript==
-javascript:with(document)(head.appendChild(createElement('script')).src='https://genbtc.github.io/AutoTrimps/AutoTrimps2.js')._;
+javascript:with(document)(head.appendChild(createElement('script')).src='https://hider-the-first.github.io/AutoTrimps/AutoAutoTrimps.js')._;
