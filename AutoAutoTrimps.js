5fcc1b6381cd0712bc9271a8a861973da0f78a98
Hider-The-First <orenvilderman@walla.co.il>
2016-08-18 12:58:15 +0300

    Update AutoAutoTrimps.js

--- AutoAutoTrimps.js
+++ AutoAutoTrimps.js
@@ -1707,7 +1707,7 @@ function autoStance() {
     	return;
     }
     if ((!game.global.mapsActive && !game.global.preMapsActive && ((hiderWindow > 10 && game.global.lastClearedCell == 98) || game.global.lastClearedCell < 98)) && (!getCurrentEnemy(1).corrupted || hiderWindow > 60 ||
-    	(!getCurrentEnemy(2).corrupted && 4*baseDamage*getPlayerCritDamageMult() > getCurrentEnemy().health/2 && hiderWindow > 5)) {
+    	(!getCurrentEnemy(2).corrupted && 4*baseDamage*getPlayerCritDamageMult() > getCurrentEnemy().health/2 && hiderWindow > 5))) {
     	setFormation(4);
     	return;
     }
@@ -2811,7 +2811,7 @@ function useScryerStance() {
         run = spirecheck ? useinspire : run;
     }
     if ((!game.global.mapsActive && !game.global.preMapsActive) && ((hiderWindow > 10 && game.global.lastClearedCell == 98) || game.global.lastClearedCell < 98)) && (!getCurrentEnemy(1).corrupted || hiderWindow > 60 ||
-    	(!getCurrentEnemy(2).corrupted && 4*baseDamage*getPlayerCritDamageMult() > getCurrentEnemy().health/2 && hiderWindow > 5)) {
+    	(!getCurrentEnemy(2).corrupted && 4*baseDamage*getPlayerCritDamageMult() > getCurrentEnemy().health/2 && hiderWindow > 5))) {
     	setFormation(4);
     	return;
     }
