e95bbd4f735e1ad8cf5b030707f625ad50f4ace6
Hider-The-First <orenvilderman@walla.co.il>
2016-08-21 23:03:20 +0300

    Testing the talking trimps here

--- user.js
+++ user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         AutoTrimps-genBTC
-// @namespace    https://github.com/genbtc/AutoTrimps
-// @version      2.1
+// @name         Testing script
+// @namespace    https://github.com/Hider-The-First/AutoTrimps
+// @version      1
 // @description  Automate all the trimps!
-// @author       zininzinin, spindrjr, Ishkaru, genBTC
+// @author       need to find out
 // @include        *trimps.github.io*
 // @include        *kongregate.com/games/GreenSatellite/trimps
 // @grant        none
 // ==/UserScript==
-javascript:with(document)(head.appendChild(createElement('script')).src='https://github.com/Hider-The-First/AutoTrimps/edit/patch-1/Experimental.js')._;
+javascript:with(document)(head.appendChild(createElement('script')).src='https://rawgit.com/Hider-The-First/AutoTrimps/patch-1/Experimental.js')._;
