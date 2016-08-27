28fd76fa177b516ca0bdf3abf7f61088bdbe7030
Hider-The-First <orenvilderman@walla.co.il>
2016-08-27 20:25:29 +0300

    Update Experimental.js

--- Experimental.js
+++ Experimental.js
@@ -42,7 +42,8 @@ document.getElementById("metal").style = "background: rgb(0, 0, 0);";
 document.getElementById("gems").style = "background: rgb(0, 0, 0);";
 document.getElementById("trimps").style = "background: rgb(0, 0, 0);";
 
-document.getElementById("battleContainer").style =  "width: 20%;" id="pic"><img style="max-height: 13vw;" src="https://catbox.moe/pictures/qts/1468421480662.png"></div><div
+//document.getElementById("battleContainer").style =  "width: 20%;" id="pic"><img style="max-height: 13vw;" src="https://catbox.moe/pictures/qts/1468421480662.png"></div><div
+document.getElementById("battleContainer").insertAdjacentHTML('beforebegin', '<div id="pic"><img style="max-height: 13vw;" src="https://catbox.moe/pictures/qts/1468421480662.png"></div><div');
 
 
 //setup talk button
