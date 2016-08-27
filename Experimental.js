667913f3bcdc626e6f6222c1160a064009799ece
Hider-The-First <orenvilderman@walla.co.il>
2016-08-27 23:09:27 +0300

    Update Experimental.js

--- Experimental.js
+++ Experimental.js
@@ -21,11 +21,11 @@ document.getElementById("gems").style = "background: rgb(0, 0, 0);";
 document.getElementById("trimps").style = "background: rgb(0, 0, 0);";
 
 //document.getElementById("battleContainer").style =  "width: 20%;" id="pic"><img style="max-height: 13vw;" src="https://catbox.moe/pictures/qts/1468421480662.png"></div><div
-document.getElementById("science").insertAdjacentHTML('afterbegin', '<div id="pic"><img style="max-height: 4vw; float: left; background;" src="http://klubznaniy.ru/%D0%B1%D0%B8%D0%BE%20%D0%B5%D0%B3%D1%8D%2015.png"></div><div');
-document.getElementById("food").insertAdjacentHTML('afterbegin', '<div id="pic"><img style="max-height: 3vw; float: left; background;" src="http://www.iconsplace.com/icons/preview/white/cooking-pot-256.png"></div><div');
-document.getElementById("wood").insertAdjacentHTML('afterbegin', '<div id="pic"><img style="max-height: 4vw; float: left; background;" src="https://www.drevomorava.cz/ftp/images/4.png"></div><div');
+document.getElementById("science").insertAdjacentHTML('afterbegin', '<div id="pic"><img style="max-height: 3vw; float: left; background;" src="http://klubznaniy.ru/%D0%B1%D0%B8%D0%BE%20%D0%B5%D0%B3%D1%8D%2015.png"></div><div');
+document.getElementById("food").insertAdjacentHTML('afterbegin', '<div id="pic"><img style="max-height: 2vw; float: left; background;" src="http://www.iconsplace.com/icons/preview/white/cooking-pot-256.png"></div><div');
+document.getElementById("wood").insertAdjacentHTML('afterbegin', '<div id="pic"><img style="max-height: 3vw; float: left; background;" src="https://www.drevomorava.cz/ftp/images/4.png"></div><div');
 //document.getElementById("metal").insertAdjacentHTML('afterbegin', '<div id="pic"><img style="max-height: 2vw; float: left; background;" src="http://game-icons.net/icons/lorc/originals/svg/anvil-impact.svg"></div><div');
-document.getElementById("trimps").insertAdjacentHTML('afterbegin', '<div id="pic"><img style="max-height: 3.5vw; float: left; background;" src="http://www.freeiconspng.com/uploads/building-city-town-icon-png--3.png"></div><div');
+document.getElementById("trimps").insertAdjacentHTML('afterbegin', '<div id="pic"><img style="max-height: 2vw; float: left; background;" src="http://www.freeiconspng.com/uploads/building-city-town-icon-png--3.png"></div><div');
 //document.getElementById("queueContainer").insertAdjacentHTML('beforeend', '<div id="pic"><img style="max-height: 2vw; float: left; background;" src="https://cdn1.iconfinder.com/data/icons/construction-and-renovation/80/Construction_renovation-12-512.png"></div><div');
 
 //beforbegin //afterbegin //beforeend //afterend
