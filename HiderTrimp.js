4422bfb3fd8e74350a95732b717b60f55d6bd439
Hider-The-First <orenvilderman@walla.co.il>
2016-08-27 07:13:47 +0300

    Update HiderTrimp.js

--- HiderTrimp.js
+++ HiderTrimp.js
@@ -11,13 +11,41 @@ document.getElementById("autotrimp").insertAdjacentHTML('beforeend', '<div style
 //Add new css rule
 //document.styleSheets[2].insertRule(".settingBtn3 {background-color: #337AB7;}", 84);
 
+
+var getPercent = 0;
+var getPercent = document.getElementById('getPercent');
+var reactPercent = 0;
+var reactPercent = document.getElementById('getPercent');
+function getStats() {
+    var getPercent = ((game.resources.helium.owned/(game.global.time/3600000))/game.global.totalHeliumEarned)*100;
+//    getPercent.innerHTML = getPercent.toFixed() + '%'; 
+    var gp = getPercent.toFixed() + '%';
+    if (getPercent.toFixed() > 5.3) {
+        var reactPercent.innerHTML = 'GRATZ, A NEW WORLD RECORD!';
+    } else if (getPercent.toFixed() > 5.2) {
+        var reactPercent.innerHTML = 'Only a few ever got this far.';
+    } else if (getPercent.toFixed() > 5.0) {
+        var reactPercent.innerHTML = 'it is not shameful to give up.';
+    } else if (getPercent.toFixed() > 4.0) {
+        var reactPercent.innerHTML = 'It is the final push.';
+    } else if (getPercent.toFixed() > 3.0) {
+        var reactPercent.innerHTML = 'Keep it comming...';
+    } else if (getPercent.toFixed() <= 3.0) {
+        var reactPercent.innerHTML = 'Did you just portal?';
+    }
+    return gp;
+}
+
 //setup convo array
 var conversation = [];
-conversation[0] = {Q:"HiThere.",R1:"My name is Minty.",L1:1,R2:"Don't take my Helium.",L2:2,R3:"Click Here.",L3:3};
+conversation[0] = {Q:"HiThere.",R1:"My name is Minty.",L1:1,R2:"Don't take my Helium.",L2:2,R3:"Tell me what to do.",L3:3};
 conversation[1] = {Q:"Thanks for the Helium.",R1:" ε(´סּ︵סּ`)з ",L1:0,};
-conversation[2] = {Q:"OK.",R1:"TY.",L1:0};
+conversation[2] = {Q:"OK.",R1:"Again.",L1:0,R2:"How am i doing so far?",L2:5,R3:"What can go wrong in the Don't Touch Zone?",L3:4};
 conversation[3] = {Q:"Please set Auto Portal to Helium Per Hour and set First Gigastation to 40 (or less). Make sure that Min Warpstation is set to two (or less). And notice that some of the Void Maps will be done before your Void Maps settings, so please use it and set the Void Maps to no more then 10 Zones before you predict that you will Auto Portal. Have a nice AutoAutoTrimps experience. ۜ \\(סּںסּَ` )/ۜ ",R1:" óÔÔò ʕ·͡ᴥ·ʔ óÔÔò ",L1:0};
-conversation[4] = {Q:"OK2.",R1:"TY.",L0:0};
+conversation[4] = {Q:"You.",R1:"Meh.",L1:0};
+conversation[5] = {Q:"Your corrent Helium per hour gain is " + getStats() + reactPercent.innerHTML,R1:"Cool.",L1:0,R2:"What can go wrong in the Don't Touch Zone?",L2:4,R3:"beam me up scotty.",L3:6};
+conversation[6] = {Q:"There's an intelligent life down here.",R1:"Lies.",L1:7};
+conversation[7] = {Q:"Never admit!",R1:"Scotty!!.",L1:0};
 updateConvo(0);
 
 /*
