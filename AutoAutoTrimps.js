c3f50d101db62a213d4de33ce93a7389f84acb0b
Hider-The-First <orenvilderman@walla.co.il>
2016-08-27 04:32:37 +0300

    Update AutoAutoTrimps.js

--- AutoAutoTrimps.js
+++ AutoAutoTrimps.js
@@ -2807,7 +2807,7 @@ function useScryerStance() {
             hiderwindow = 100; //enoughDamage = true; enoughHealth = true; shouldFarm = false;
         }
         //quit here if its right
-        if (game.global.preMapsActive || (!game.global.preMapsActive && game.global.mapsActive && getCurrentMapObject().location != "Void") || HDratio > 7 || (game.global.spireActive && game.global.lastClearedCell > 77) || game.global.gridArray.length === 0 || game.global.highestLevelCleared < 180 || (hiderwindow < 80 && game.global.lastClearedCell == 98)) { autoStance(); return;
+        if (game.global.preMapsActive || (!game.global.preMapsActive && game.global.mapsActive && getCurrentMapObject().location == "Void") || HDratio > 7 || (game.global.spireActive && game.global.lastClearedCell > 77) || game.global.gridArray.length === 0 || game.global.highestLevelCleared < 180 || (hiderwindow < 80 && game.global.lastClearedCell == 98)) { autoStance(); return;
         }
         if (ovklHDratio > 0.6) {
             setFormation(4);
