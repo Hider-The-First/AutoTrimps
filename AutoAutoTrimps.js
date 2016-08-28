26077734a1c35d607d3aee811460e05e541c00b3
Hider-The-First <orenvilderman@walla.co.il>
2016-08-28 13:32:37 +0300

    Update AutoAutoTrimps.js

--- AutoAutoTrimps.js
+++ AutoAutoTrimps.js
@@ -1076,13 +1076,11 @@ function highlightHousing() {
     game.global.buyAmt = oldBuy;
 }
 function getNiceThingsDone() {
-	/*
-	if (game.global.preMapsActive &&  game.global.mapGridArray.length < 10) {
+	if (game.global.preMapsActive &&  game.global.gridArray.length === 0)) {
 		return;;
-	}else if (!game.global.preMapsActive && !game.global.mapsActive && game.global.gridArray.length < 10) {
+	}else if (!game.global.preMapsActive && !game.global.mapsActive && game.global.mapGridArray.length === 0)) {
 		return;;
 	}
-	*/
 	if (!game.global.preMapsActive && !game.global.mapsActive && !game.global.spireActive && (new Date().getTime() - game.global.zoneStarted) > 100 &&  game.global.GridArray.length > 5) {
 	document.getElementById("trimps").insertAdjacentHTML('afterend', '<div id="pic"><img style="max-height: 9000vw; vertical-align; background;" src="http://4hdwallpapers.com/wp-content/uploads/2014/12/road_summer-Beautiful_natural_scenery_Desktop_Wallpapers_1366x768-1024x575.jpg"></div><div');
 	var cells = document.getElementById("grid").getElementsByClassName("battleCell cellColorBeaten"); var oldstyle = cells[0].getAttribute('style'); for (var i=0; i < cells.length; i++) cells[i].setAttribute('style', oldstyle + '; background-color: rgba(0,0,0,0.3);');
