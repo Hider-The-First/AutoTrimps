b9dcf2ba7d88aac3a080b260bb11ad015946441c
Hider-The-First <orenvilderman@walla.co.il>
2016-08-28 13:02:32 +0300

    Update AutoAutoTrimps.js

--- AutoAutoTrimps.js
+++ AutoAutoTrimps.js
@@ -1076,7 +1076,9 @@ function highlightHousing() {
     game.global.buyAmt = oldBuy;
 }
 function getNiceThingsDone() {
-	if (game.global.gridArray.length < 10 || game.global.mapGridArray.length < 10) {
+	if (game.global.preMapsActive &&  game.global.mapGridArray.length < 10) {
+		return;
+	}else if (!game.global.preMapsActive && !game.global.mapsActive && game.global.gridArray.length < 10) {
 		return;
 	}
 	if (!game.global.preMapsActive && !game.global.mapsActive && !game.global.spireActive) {
