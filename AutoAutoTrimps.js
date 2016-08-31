50debc20a993a971d0c32fce82d367f9c5914555
Hider-The-First <orenvilderman@walla.co.il>
2016-09-01 01:55:08 +0300

    Update AutoAutoTrimps.js

--- AutoAutoTrimps.js
+++ AutoAutoTrimps.js
@@ -2097,7 +2097,7 @@ function autoMap() {
         (game.global.world >= 62 && !!game.buildings.Nursery.locked) ||	//Clear maps to stack up nurseries for genetics before starting warps stucking.
         //(!game.upgrades.Gigastation.locked && game.global.mapBonus < 1 && !game.global.mapsActive && (game.upgrades.Gigastation.allowed-4 >= game.upgrades.Gigastation.done)) ||	//Clear maps to stack up early warps.
         (game.global.mapBonus < 9 && hiderwindow < 0.5 ) ||	//Farm maps if you are way to slow
-        (game.global.mapsActive && getBreedTime(true) > 0 && hiderwindow < 10)	//Stay in maps to heal
+        (game.global.mapsActive && getBreedTime(true) > 0 && hiderwindow < 1)	//Stay in maps to heal
         //(game.global.world >= 310 && ((new Date().getTime() - game.global.zoneStarted) / 1000 / 60) < 10)	//option to force stay in zone X time in min/cleared maps and farm
         //(game.global.world == 200 && game.global.lastClearedCell > 20 && ((new Date().getTime() - game.global.zoneStarted) / 1000 / 60) < 10)		//option to force stay in zone X time in min and farm		
 	) {		
@@ -3138,11 +3138,13 @@ function generateHeirloomIcon(heirloom, location, number){
     abutton.id = 'hiderStatus';
     newContainer.appendChild(abutton);
     fightButtonCol.appendChild(newContainer);
+    autoautoDonttouchBtn.setAttribute("onmouseover", 'tooltip(\"OverKill Chance\", \"customText\", event, \"Farm Void when under 30/nUse D stance in maps when under 3/nIgnore high level void limits if over 9/nSave high level void maps if over 30./nGet 200% map bonus if under 2.5/nGet +20% map bonus if in map and breeding if under 3.\")');
+    autoautoDonttouchBtn.setAttribute("onmouseout", 'tooltip("hide")');
 
 var hiderwindow = 0;
 var hiderWindow = document.getElementById('hiderWindow');
 function updateValueFields2() {
     var hiderWindow = document.getElementById('hiderWindow');
     var hiderStatus = document.getElementById('hiderStatus');
-    hiderStatus.innerHTML = 'OverKill Chance ' + hiderwindow.toFixed(2) + '%'; 
+    hiderStatus.innerHTML = 'OverKill Chance ' + hiderwindow.toFixed(2)*5 + '%'; 
 }
