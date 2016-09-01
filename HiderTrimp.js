2f2a675aa95640565689c16392645bb246698641
Hider-The-First <orenvilderman@walla.co.il>
2016-09-01 09:08:26 +0300

    Update HiderTrimp.js

--- HiderTrimp.js
+++ HiderTrimp.js
@@ -184,7 +184,7 @@ if (checking != null && checking.versioning == version) {
 	}else {
 	var BR = {enabled: 1, description: "Background RGB R", titles: ["-1", "X"]};
 	var BG = {enabled: 0, description: "Background RGB G", titles: ["Not Buying", "Buying Both", "Gyms Only", "Tributes Only"]};
-	var BB = {enabled: 1, description: "Background RGB B, titles: ["Not Reading", "Reading", "Reading and Weapons", "Reading and Equipment", "Reading and Armour"]};
+	var BB = {enabled: 1, description: "Background RGB B", titles: ["Not Reading", "Reading", "Reading and Weapons", "Reading and Equipment", "Reading and Armour"]};
 	var CR = {enabled: 0, description: "Containers RGB R", titles: ["Not Buying", "Buying Both", "Houses Only", "Nurseries Only"]};
 	var CG = {enabled: 0, description: "Containers RGB G", titles: ["Not Jobbing", "Jobbing"]};
 	var CB = {enabled: 0, description: "Containers RGB B", titles: ["Not Highlighting", "Highlighting All", "Housing Only", "Equipment Only"]};
