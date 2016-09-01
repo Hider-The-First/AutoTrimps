a1d510bb291719ddc4078c87df6f5670102abb00
Hider-The-First <orenvilderman@walla.co.il>
2016-09-01 03:08:33 +0300

    Update AutoAutoTrimps.js

--- AutoAutoTrimps.js
+++ AutoAutoTrimps.js
@@ -995,7 +995,7 @@ function safeBuyBuilding(building) {
     game.global.firing = false;
     //avoid slow building from clamping
     //buy as many warpstations as we can afford but buys the last warpstations in single.
-    if (bestBuilding == "Warpstation" && getBuildingItemPrice(game.buildings.Warpstation, "metal", false, 1) * Math.pow(1 - game.portal.Resourceful.modifier, game.portal.Resourceful.level) > game.resources.metal.owned/(4+costratio)) {
+    if (bestBuilding == "Warpstation" && getBuildingItemPrice(game.buildings.Warpstation, "metal", false, 1) * Math.pow(1 - game.portal.Resourceful.modifier, game.portal.Resourceful.level) > game.resources.metal.owned/5) {
         game.global.buyAmt = 1;
     } else {
     	game.global.buyAmt = 'Max';
