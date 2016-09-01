752c9c2508fbc265118b615c1843848fcc3e3fc4
Hider-The-First <orenvilderman@walla.co.il>
2016-09-01 04:36:05 +0300

    Update HiderTrimp.js

--- HiderTrimp.js
+++ HiderTrimp.js
@@ -32,7 +32,7 @@ document.getElementById("trimps").style = "background: rgb(0, 0, 0);";
 //beforbegin //afterbegin //beforeend //afterend
 
 //setup talk button
-letMeTalk = document.getElementById("buildingsCollectBtn")
+letMeTalk = document.getElementById("buildingsCollectBtn");
 document.getElementById("buildingsQueue").style = "width: 75%; float: left;";
 document.getElementById("queueContainer").insertAdjacentHTML('beforeend', '<div style="color: rgb(255, 255, 255); font-size: 1.2em; text-align: center; width: 20%; float: right; vertical-align;"><div id="buildingsCollectBtn" class="workBtn pointer noselect" onclick="talk()" style="background: rgb(0, 0, 0) none repeat scroll 0% 0%; margin-top: 0.5vh;">Talk</div></div>');
 letMeTalk.setAttribute("onmouseover", 'tooltip(\"Talk\", \"customText\", event, \"He knows a lot about how Trimps works.\")');
