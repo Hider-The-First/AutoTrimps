fe6ba2c8df3e7b7aa55aa0482f00426ec364ce5e
Hider-The-First <orenvilderman@walla.co.il>
2016-09-02 01:04:04 +0300

    Update HiderTrimp.js

--- HiderTrimp.js
+++ HiderTrimp.js
@@ -5,15 +5,15 @@ var autoTSettings = {};
 var version = "0.37b.17T2";
 
 //nice globals
-var zonePic = 0;
+var zonePic = null;
 var zonePic = document.getElementById('zonePic');
-var PrePic = 0;
+var PrePic = null;
 var PrePic = document.getElementById('PrePic');
-var voidPic = 0;
+var voidPic = null;
 var voidPic = document.getElementById('voidPic');
-var mapPic = 0;
+var mapPic = null;
 var mapPic = document.getElementById('mapPic');
-var spirePic = 0;
+var spirePic = null;
 var spirePic = document.getElementById('spirePic');
 var BR = 0;
 var BR = document.getElementById('BR');
@@ -49,23 +49,23 @@ document.getElementById("queueContainer").insertAdjacentHTML('beforebegin', '<di
 //beforebegin //afterbegin //beforeend //afterend
 
 function getNiceThingsDone() {
-		if (zonePic > 0 || PrePic > 0 || voidPic > 0 || mapPic > 0 || spirePic > 0) {
+		if (zonePic != null || PrePic != null || voidPic != null || mapPic != null || spirePic != null ) {
 		//bring the art.
-		if (zonePic> 0 && !game.global.preMapsActive && !game.global.mapsActive && !game.global.spireActive) {
+		if (zonePic != null && !game.global.preMapsActive && !game.global.mapsActive && !game.global.spireActive) {
 		document.getElementById("trimps").insertAdjacentHTML('afterend', '<div id="pic"><img src="' + document.getElementById("zonePic").value + '"></div>');
-		} else if (PrePic > 0  && game.global.preMapsActive) {
+		} else if (PrePic != null && game.global.preMapsActive) {
 		document.getElementById("trimps").insertAdjacentHTML('afterend', '<div id="pic"><img src="' + document.getElementById("PrePic").value + '"></div>');
-		} else if (voidPic > 0 && game.global.mapsActive && getCurrentMapObject().location == "Void") {
+		} else if (voidPic != null && game.global.mapsActive && getCurrentMapObject().location == "Void") {
 		document.getElementById("trimps").insertAdjacentHTML('afterend', '<div id="pic"><img src="' + document.getElementById("voidPic").value + '"></div>');
-		} else if (mapPic > 0 && game.global.mapsActive && getCurrentMapObject().location != "Void") {
+		} else if (mapPic != null && game.global.mapsActive && getCurrentMapObject().location != "Void") {
 		document.getElementById("trimps").insertAdjacentHTML('afterend', '<div id="pic"><img src="' + document.getElementById("mapPic").value + '"></div>');
-		} else if (spirePic > 0 && game.global.world == 200 && game.global.spireActive) {
+		} else if (spirePic != null && game.global.world == 200 && game.global.spireActive) {
 		document.getElementById("trimps").insertAdjacentHTML('afterend', '<div id="pic"><img src="' + document.getElementById("spirePic").value + '"></div>');
 		}
-		if ((spirePic > 0 || zonePic > 0) && game.resources.trimps.soldiers != 0 && !game.global.preMapsActive && !game.global.mapsActive && (new Date().getTime() - game.global.zoneStarted) > 1600 && game.global.gridArray.length != 0) {
+		if ((spirePic != null || zonePic != null ) && game.resources.trimps.soldiers != 0 && !game.global.preMapsActive && !game.global.mapsActive && (new Date().getTime() - game.global.zoneStarted) > 1600 && game.global.gridArray.length != 0) {
 			var cells = document.getElementById("grid").getElementsByClassName("battleCell cellColorBeaten"); var oldstyle = cells[0].getAttribute('style'); for (var i=0; i < cells.length; i++) cells[i].setAttribute('style', oldstyle + '; background-color: rgba(0,0,0,0.3);');
 		}
-		if ((mapPic > 0  || voidPic > 0) && game.resources.trimps.soldiers != 0 && game.global.mapsActive && (new Date().getTime() - game.global.mapStarted) > 1600 && game.global.mapGridArray.length != 0) {
+		if ((mapPic != null || voidPic != null ) && game.resources.trimps.soldiers != 0 && game.global.mapsActive && (new Date().getTime() - game.global.mapStarted) > 1600 && game.global.mapGridArray.length != 0) {
 			var cells = document.getElementById("mapGrid").getElementsByClassName("battleCell cellColorBeaten"); var oldstyle = cells[0].getAttribute('style'); for (var i=0; i < cells.length; i++) cells[i].setAttribute('style', oldstyle + '; background-color: rgba(0,0,0,0.3);');
 		}
 	}
