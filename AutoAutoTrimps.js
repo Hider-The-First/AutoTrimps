b848422bf236ca9d2e72cb1ea1792b31fe7a62d2
Hider-The-First <orenvilderman@walla.co.il>
2016-09-03 06:13:46 +0300

    Update AutoAutoTrimps.js

--- AutoAutoTrimps.js
+++ AutoAutoTrimps.js
@@ -3147,5 +3147,5 @@ function updateValueFields3() {
     var Area51 = document.getElementById('Area51');
     var Area52 = document.getElementById('Area52');
     var Area60 = document.getElementById('Area60');
-    Area52.innerHTML = 'Home:' + Area51 + '<br>Guest:' + Area60; 
+    Area52.innerHTML = 'Home:' + (Area51).toFixed(2) + '<br>Guest:' + (Area60).toFixed(2); 
 }
