c20543f818306c7f6e15e5ee777355585616a8f4
Hider-The-First <orenvilderman@walla.co.il>
2016-09-05 22:36:33 +0300

    Update AutoAutoTrimps.js

--- AutoAutoTrimps.js
+++ AutoAutoTrimps.js
@@ -1525,7 +1525,7 @@ function autoLevelEquipment() {
                     || (gameResource.prestige+4 <= (game.global.world-5)/5 && game.global.soldierHealth > 0 && armorTempValue < 50)
                     || (gameResource.prestige+3 <= (game.global.world-5)/5 && game.global.soldierHealth > 0 && armorTempValue < 20)
                     || (gameResource.prestige+2 <= (game.global.world-5)/5 && game.global.soldierHealth > 0 && armorTempValue < 10)
-                    || armorValue < 1000)) || gameResource.prestige < 5 || game.global.world == 200 ) && (equipmentList[equipName].Stat == 'health'))
+                    || (game.global.soldierHealth > 0 && armorValue < 1000))) || gameResource.prestige < 5 || game.global.world == 200 ) && (equipmentList[equipName].Stat == 'health'))
                         && 
                 //Only buy Armor prestiges when 'DelayArmorWhenNeeded' is on, IF:
                         (
