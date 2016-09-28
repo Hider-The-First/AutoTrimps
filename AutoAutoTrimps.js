ee8fea60284c13fa1e28bf02667fd87999ef145d
Hider-The-First <orenvilderman@walla.co.il>
2016-09-29 02:40:14 +0300

    Update AutoAutoTrimps.js

--- AutoAutoTrimps.js
+++ AutoAutoTrimps.js
@@ -3193,12 +3193,12 @@ function displaySelectedHeirloom(modSelected, selectedIndex, fromTooltip, locati
     if (fromPopup && !game.options.menu.voidPopups.enabled) return;
     var heirloom = getSelectedHeirloom(locationOvr, indexOvr);
     var icon = getHeirloomIcon(heirloom);
-    var html = '<div class="selectedHeirloomItem heirloomRare' + heirloom.rarity + '"><div class="row selectedHeirloomRow"><div class="col-xs-2 selectedHeirloomIcon"><span style="max-height: 5vw"; class="' + icon + '"></span></div><div class="col-xs-10"><span onclick="renameHeirloom(';
+    var html = '<div class="selectedHeirloomItem heirloomRare' + heirloom.rarity + '"><div class="row selectedHeirloomRow"><div class="col-xs-2 selectedHeirloomIcon"><span style="max-height: 5vw"; style="max-width: 5vw"; class="' + icon + '"></span></div><div class="col-xs-10"><span onclick="renameHeirloom(';
     if (fromPopup) html += 'false, true';
     html += ')" id="selectedHeirloomTitle">' + heirloom.name + '</span> '
     if (!fromTooltip) html += '<span id="renameContainer"></span>';
     html+= '</div></div>';
-    if (!fromTooltip && (game.global.selectedHeirloom[1] == "StaffEquipped" || game.global.selectedHeirloom[1] == "ShieldEquipped")) html += '<span style="max-height: 8vw"; class="heirloomEquipped">Equipped</span><br/>';
+    if (!fromTooltip && (game.global.selectedHeirloom[1] == "StaffEquipped" || game.global.selectedHeirloom[1] == "ShieldEquipped")) html += '<span style="max-height: 8vw"; style="max-width: 8vw"; class="heirloomEquipped">Equipped</span><br/>';
     var noneEmpty = true;
     var opacity = (modSelected) ? 'style="opacity: 0.5" ' : '';
     for (var x = 0; x < heirloom.mods.length; x++){
